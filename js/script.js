284e61e8941955967fa6870dc8e4bc77f69a0538
angel de jesus <a20210014@utem.edu.mx>
2024-02-19 11:49:22 -0600

    cambios2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 function cargarImagen() {
-    const startTime = performance.now(); // Inicia el contador de tiempo
+    const startTime = performance.now(); 
     const contenedorImagen = document.getElementById('contenedorImagen');
     const imagen = new Image();
 
     imagen.onload = function() {
-        const endTime = performance.now(); // Finaliza el contador de tiempo
-        const tiempoTranscurrido = (endTime - startTime) / 1000; // Convertir de milisegundos a segundos
+        const endTime = performance.now(); 
+        const tiempoTranscurrido = (endTime - startTime) / 1000; 
         document.getElementById('contadorTiempo').innerText = `Tiempo transcurrido: ${tiempoTranscurrido.toFixed(2)} segundos`;
 
         contenedorImagen.appendChild(imagen);
     };
 
-    imagen.src = './imagen/imagen1.jpg'; // Coloca aquí la URL de la imagen que deseas cargar
+    imagen.src = './imagen/imagen1.jpg'; 
 }
